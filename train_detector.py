39a965b572e4ea6c29935f383b956195403fb590
Sai Kiran Burle <sai@celect.com>
2018-03-11 11:27:40 -0400

    add bottleneck layer

--- train_detector.py
+++ train_detector.py
@@ -81,7 +81,8 @@ def get_model():
     x = base_model.output
 
     # Use output to determine in/out dist
-    x = Dense(units=50, activation='relu')(x)
+    x = Dense(units=30, activation='relu')(x)
+    x = Dense(units=5, activation='relu')(x)
     x = Dense(units=10, activation='relu')(x)
     predictions = Dense(2, activation='softmax')(x)
 
